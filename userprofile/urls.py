9f84f95740ec53230227fdab302f104a4d3d6c68
Manish <smanish@code.com>
2023-06-22 18:15:28 +0530

    Update cart wishlist in 'userprofile

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("website/api/", include("userprofile.website.urls")),
+    path("app/api/", include("userprofile.app.urls")),
+    path("dashboard/api/", include("userprofile.dashboard.urls")),
 ]
+
